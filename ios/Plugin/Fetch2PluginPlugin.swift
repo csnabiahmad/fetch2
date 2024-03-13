ec6bcdda7d67e4c15dd9dc7b00c8df160030aa82
Nabi Ahmad <cs.nabiahmad@gmail.com>
2024-03-13 15:18:22 +0500

    Cdoe Clean

--- ios/Plugin/Fetch2PluginPlugin.swift
+++ ios/Plugin/Fetch2PluginPlugin.swift
@@ -1,25 +1,9 @@
 import Foundation
 import Capacitor
-
-/**
- * Please read the Capacitor iOS Plugin Development Guide
- * here: https://capacitorjs.com/docs/plugins/ios
- */
+ 
 @objc(Fetch2PluginPlugin)
 public class Fetch2PluginPlugin: CAPPlugin /*, URLSessionDownloadDelegate , DownloadDelegate*/ {
-//    public func didFinish(_ donwloads: Download?) {
-//        print("Fetch2PluginPlugin => Donwload:: \(String(describing: donwloads))")
-//     }
-//    
-//    public func didError(_ donwloads: Download?) {
-//        print("Fetch2PluginPlugin => Donwload:: \(String(describing: donwloads))")
-//    }
-//    
-//    public func didProgress(_ donwloads: Download?) {
-//        print("Fetch2PluginPlugin => Donwload:: \(String(describing: donwloads))")
-//       
-//    }
-    
+  
     private let implementation = Fetch2Plugin()
     private var downloadManager : DownloadManager!
 
@@ -51,92 +35,76 @@ public class Fetch2PluginPlugin: CAPPlugin /*, URLSessionDownloadDelegate , Down
     }
     
     @objc func startFetch(_ call: CAPPluginCall) {
-//        guard let url = call.getString("url") else { return call.reject("Must provide a URL") }
         guard let urls = call.getArray("url", String.self) else {
-            call.reject("No url provided")
+            call.reject("No valid urls provided")
             return
         }
-        for url in urls{
-            let progressEmitter: Fetch2Plugin.ProgressEmitter = { bytes, contentLength in
-                let progress = (Double(bytes) / Double(contentLength))*100
+        guard let downloads = self.implementation.fetchDownloads() else { return }
 
-                CAPLog.print("Progress",String(describing: progress))
-                CAPLog.print("URL",String(describing: url))
+        for url in urls{
+            let contains = downloads.first{ download in
+                download.url == url
+            }
+            
+            if contains == nil {
                 
-                if (Int(progress) % 10) == 0 {
-                    if let index = self.implementation.downloadList?.firstIndex(where: { $0.url == url }) {
-                        if var newDownload = self.implementation.downloadList?[index]{
-//                            newDownload.progress = progress
-                            newDownload.downloaded = Int(bytes)
-                            newDownload.total = Int(contentLength)
-                            self.implementation.downloadList?[index] = newDownload
-                            self.implementation.saveDownloads()
-                            
-                            let encoder = JSONEncoder()
-                            encoder.outputFormatting = .prettyPrinted
-                            do {
-                                let jsonData = try encoder.encode(newDownload)
-                                if let jsonString = String(data: jsonData, encoding: .utf8) {
-                                    CAPLog.print(jsonString)
-                                    if(Int(bytes) == Int(contentLength)){
-                                        self.notifyListeners("onCompleted", data:["download": jsonString])
-                                    }else{
-                                        self.notifyListeners("onProgress", data:["download": jsonString])
+                let progressEmitter: Fetch2Plugin.ProgressEmitter = { bytes, contentLength in
+                    let progress = (Int(bytes) / Int(contentLength)) * 100
+                    
+                    CAPLog.print("Progress",String(describing: progress))
+                    CAPLog.print("URL",String(describing: url))
+                    
+                    if (progress % 10) == 0 {
+                        if let index = self.implementation.downloadList?.firstIndex(where: { $0.url == url }) {
+                            if var newDownload = self.implementation.downloadList?[index]{
+                                newDownload.downloaded = Int(bytes)
+                                newDownload.total = Int(contentLength)
+                                newDownload.status = progress == 100 ? "COMPLETED" : "DONWLOADING"
+                                self.implementation.downloadList?[index] = newDownload
+                                self.implementation.saveDownloads()
+                                
+                                let encoder = JSONEncoder()
+                                encoder.outputFormatting = .prettyPrinted
+                                do {
+                                    let jsonData = try encoder.encode(newDownload)
+                                    if let jsonString = String(data: jsonData, encoding: .utf8) {
+                                        CAPLog.print(jsonString)
+                                        if(Int(bytes) == Int(contentLength)){
+                                            
+                                            self.notifyListeners("onCompleted", data:["download": jsonString])
+                                        }else{
+                                            self.notifyListeners("onProgress", data:["download": jsonString])
+                                        }
                                     }
+                                } catch {
+                                    CAPLog.print("Error encoding Download: \(error)")
                                 }
-                            } catch {
-                                CAPLog.print("Error encoding Download: \(error)")
+                                
                             }
-                            
                         }
                     }
                 }
+                do {
+                    if let urlString = URL(string: url){
+                        try implementation.downloadFile(call: call,url: urlString, emitter: progressEmitter, config: bridge?.config)
+                    }else {return}
+                } catch let error {
+                    call.reject(error.localizedDescription)
+                }
             }
-            do {
-                if let urlString = URL(string: url){
-                    try implementation.downloadFile(call: call,url: urlString, emitter: progressEmitter, config: bridge?.config)
-                }else {return}
-            } catch let error {
-                call.reject(error.localizedDescription)
+            else{
+                let encoder = JSONEncoder()
+                encoder.outputFormatting = .prettyPrinted
+                do {
+                    let jsonData = try encoder.encode(contains)
+                    if let jsonString = String(data: jsonData, encoding: .utf8) {
+                    CAPLog.print(jsonString)
+                    self.notifyListeners("onCompleted", data:["download": jsonString])
+                    }
+                } catch {
+                    CAPLog.print("Error encoding Download: \(error)")
+                }
             }
         }
     }
-    
-//    @objc func startFetch(_ call: CAPPluginCall) {
-//        guard let urls = call.getArray("url", String.self) else {
-//            call.reject("No url provided")
-//            return
-//        }
-//            var ret = [String: Any]()
-//            ret["value"] = urls
-//            downloadManager = DownloadManager()
-//            downloadManager?.downloadDelegate = self
-//        do {
-//            try implementation.startFetch(urls)
-//        } catch {
-//            ret["error"] = error.localizedDescription
-//        }
-//        call.resolve(ret)
-//    }
-    
-    // MARK: URLSessionDownloadDelegate methods
-    
-//    public func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
-//        print("Download finished: \(location)")
-//        // Here you can move the downloaded file from `location` to your desired directory
-//    }
-//    
-//    public func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
-//        if error != nil {
-//            print("Download failed with error: \(String(describing: error?.localizedDescription))")
-//        }
-//    }
-//    
-//    public func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
-//        if totalBytesExpectedToWrite > 0 {
-//            let progress = Int(totalBytesWritten) / Int(totalBytesExpectedToWrite) * 100
-//            print("Progress: \(progress)%")
-//            self.notifyListeners("onProgress", data: ["download" : "\(progress)"])
-//        }
-//    }
 }
